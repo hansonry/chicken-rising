f920c949aac78f8bcaae7374bad3f16a3bcb1547
Ryan Hanson <hansonry@gmail.com>
2023-05-13 16:43:32 -0500

    Added chicken

--- player.gd
+++ player.gd
@@ -4,18 +4,25 @@ extends CharacterBody2D
 const SPEED = 100.0
 
 func _physics_process(delta):
-
+	var moving :bool = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("Left", "Right")
 	var directionY = Input.get_axis("Up", "Down")
 	if directionX:
+		moving = true
 		velocity.x = directionX * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if directionY:
+		moving = true
 		velocity.y = directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if moving:
+		$AnimatedSprite2D.play("Walk")
+	else:
+		$AnimatedSprite2D.play("Idel")
 
 	move_and_slide()
