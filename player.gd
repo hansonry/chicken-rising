ae189b5bb63e22ec633f903dd73804fcc2a3fffb
Ryan Hanson <hansonry@gmail.com>
2023-05-13 17:23:59 -0500

    Added new chicken sprites and coded the directions

--- player.gd
+++ player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
+var last_direction: String = "Right"
 
 const SPEED = 100.0
 
 func _physics_process(delta):
 	var moving :bool = false
+	var mirror :bool = false
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("Left", "Right")
@@ -12,17 +14,25 @@ func _physics_process(delta):
 	if directionX:
 		moving = true
 		velocity.x = directionX * SPEED
+		last_direction = "Right"
+		if directionX < 0:
+			mirror = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if directionY:
 		moving = true
 		velocity.y = directionY * SPEED
+		if directionY > 0:
+			last_direction = "Down"
+		else:
+			last_direction = "Up"
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	if moving:
-		$AnimatedSprite2D.play("Walk")
+		$AnimatedSprite2D.play("Walk" + last_direction)
+		$AnimatedSprite2D.flip_h = mirror
 	else:
-		$AnimatedSprite2D.play("Idel")
+		$AnimatedSprite2D.play("Idel" + last_direction)
 
 	move_and_slide()
