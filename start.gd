908a6426888d6275aa8f43e3265bd25bb94cd2f2
Ryan Hanson <hansonry@gmail.com>
2023-05-14 12:33:11 -0500

    Added constant for room transisiton speed. Normalized chicking speed so diaganal speed wont be faster

--- start.gd
+++ start.gd
@@ -3,6 +3,8 @@ extends Node2D
 const WIDTH  : int = 320
 const HEIGHT : int = 240
 
+const ROOM_TRANSITION_SPEED: float = 20
+
 var camera_destination: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +22,7 @@ func _process(delta):
 	var camera : Camera2D = $Camera2D
 	var player: CharacterBody2D = $Player
 	camera_destination = _compute_camera_location(player.transform.origin)
-	camera.transform.origin = camera.transform.origin.lerp(camera_destination, delta * 20)
+	camera.transform.origin = camera.transform.origin.lerp(camera_destination, delta * ROOM_TRANSITION_SPEED)
 	
 
 
