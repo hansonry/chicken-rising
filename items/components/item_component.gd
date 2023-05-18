73fd4bb87d4b9fa46ec66d6aafdb0f972c5c9088
Ryan Hanson <hansonry@gmail.com>
2023-05-17 20:00:47 -0500

    Refactored all the pickups as resources. I think this will make more sense

---
+++ items/components/item_component.gd
@@ -0,0 +1,6 @@
+class_name  ItemComponent
+extends Item
+
+@export var identified : bool = false
+@export var identified_by : ItemBook = null
+
--- pickupable.gd
+++ pickupable.gd
@@ -1,9 +1,26 @@
+@tool
 extends Area2D
 
 var picked_up : bool = false
 
+func _setup_item():
+		if item != null:
+			$SoundPickup.stream = item.sound_on_pickup
+			$Sprite2D.texture = item.texture
+		else:
+			$SoundPickup.stream = null
+			$Sprite2D.texture = null
+	
+
+@export var item : Item = null :
+	set(new_item):
+		item = new_item
+		_setup_item()
+		
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_setup_item()
 	pass # Replace with function body.
 
 
@@ -20,6 +37,7 @@ func _pick_up():
 	if not picked_up:
 		_play_pickup_sound()
 		hide()
+		Inventory.add_item(ItemComponent.new("Circuit board"))
 		picked_up = true
 	
 	
