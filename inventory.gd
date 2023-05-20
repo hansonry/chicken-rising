18e73ac5e7a497c6cec6307121250e86fe6af59d
Ryan Hanson <hansonry@gmail.com>
2023-05-20 17:02:41 -0500

    Got a Victory Printout Working. Incubator can now be built

--- inventory.gd
+++ inventory.gd
@@ -4,12 +4,34 @@ var items := []
 
 var notifier: Notifier = null
 @onready var stealth_box_scene : Resource = load ("res://items/stealth_box/cardboard_box.tscn")
+@onready var _incubator_requirements : IncubatorRequirements = preload("res://items/incubator_requirements.tres")
+var _can_build_incubator : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func game_ready():
 	notifier = get_node("/root/Node2D/Camera2D/Notifier")
+
+func _find_in_inventory(item_to_find: Item) -> bool:
+	for item in items:
+		if item == item_to_find:
+			return true
+	return false
+
+func _check_if_can_build_incubator():
+	var can_build : bool = true
+	for r_comp in _incubator_requirements.components:
+		if not _find_in_inventory(r_comp) or not r_comp.identified:
+			can_build = false
+			break
+	if can_build:
+		print("Can build Incubator")
+		_can_build_incubator = true	
+
+func can_build_incubator() -> bool:
+	return _can_build_incubator
 
 func _component_identified(component: ItemComponent, book: ItemBook):
 	print("Identifed! " + component.name)
@@ -19,6 +41,7 @@ func _component_identified(component: ItemComponent, book: ItemBook):
 	notification.image = component.texture
 	notification.sound = component.sound_on_pickup
 	notifier.add_notification(notification)
+	_check_if_can_build_incubator()
 	
 func _identify_components_using_book(book: ItemBook):
 	for item in items:
@@ -54,6 +77,7 @@ func _notify_box_got(component: ItemEquipment):
 
 func _add_box_to_player():
 	get_tree().current_scene.find_child("Player").add_child( stealth_box_scene.instantiate() )
+
 
 
 
---
+++ items/incubator_requirements.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name IncubatorRequirements
+
+@export var components : Array[ItemComponent] = []
