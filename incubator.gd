97f268996dda49c5bcb8384c57171c5ce5c30e7d
Ryan Hanson <hansonry@gmail.com>
2023-05-20 18:35:47 -0500

    Incubator is now built

--- incubator.gd
+++ incubator.gd
@@ -1,19 +1,27 @@
 extends Area2D
 
+var _started_build : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Incubator.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func _start_build_process():
+	_started_build = true
+	$BuildingSound.play()
 
 func _attempt_to_build():
-	if Inventory.can_build_incubator():
-		print("Incubator Built!")
+	if Inventory.can_build_incubator() and not _started_build:
+		_start_build_process()
 
 func _on_body_entered(body):
 	_attempt_to_build()
+
+
+func _on_building_sound_finished():
+	$Incubator.visible = true
